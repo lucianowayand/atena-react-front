f69fc87ea4bf767d143f7ea3f68df23a61f97fbe
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-06-28 11:24:41 -0300

    mui and some css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import SecondPage from "./components/SecondPage";
 import ThirdPage from "./components/ThirdPage";
 
 export default function App() {
+  const [currentComponent, setComponent] = useState('FirstPage')
+  
   function dynamicRendering() {
     if (currentComponent == 'FirstPage') {
       setComponent('SecondPage')
@@ -16,9 +18,8 @@ export default function App() {
     }
   }
 
-  const [currentComponent, setComponent] = useState('FirstPage')
   return (
-    <div>
+    <div className="login-form">
       <h1>Atena.io - Initial commit</h1>
 
       {currentComponent == 'FirstPage' ?
